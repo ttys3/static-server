f7e45240599f3202a5f32857d6f884249c134f36
荒野無燈 <ttys3@outlook.com>
2021-08-21 20:34:33 +0800

    chore: listen on any card

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ async fn main() {
         .route("/favicon.ico", get(favicon))
         .layer(TraceLayer::new_for_http());
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::info!("listening on http://{}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
