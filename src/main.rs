02862b87b87da61d9e9caaedfbaab2d10a1441b5
ttyS3 <ttys3.rust@gmail.com>
2022-03-25 00:15:05 +0800

    build: update to https://github.com/tokio-rs/axum.git?branch=main#56e2d573

--- src/main.rs
+++ src/main.rs
@@ -109,9 +109,7 @@ async fn favicon() -> impl IntoResponse {
     // one pixel favicon generated from https://png-pixel.com/
     let one_pixel_favicon = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPk+89QDwADvgGOSHzRgAAAAABJRU5ErkJggg==";
     let pixel_favicon = base64::decode(one_pixel_favicon).unwrap();
-    let mut res = Response::new(Full::from(pixel_favicon));
-    res.headers_mut().insert(header::CONTENT_TYPE, HeaderValue::from_static("image/png"));
-    res
+    ([(header::CONTENT_TYPE, HeaderValue::from_static("image/png"))], pixel_favicon)
 }
 
 // Request<Body> used an extractors cannot be combined with other unless Request<Body> is the very last extractor.
