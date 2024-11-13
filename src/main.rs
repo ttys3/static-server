894db85b363ff7faf5553e8d2c9f543c9e2d1809
ttyS3 <ttys3.rust@gmail.com>
2024-11-13 06:44:31 +0000

    style: adjust code formatting in main.rs

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,13 @@ use std::str::FromStr;
 use tokio::process::Command;
 
 #[derive(Parser, Debug)]
-#[clap(name = "static-server", about = "A simple static file server written in Rust based on axum framework.", author, version, long_about = None)]
+#[clap(
+    name = "static-server",
+    about = "A simple static file server written in Rust based on axum framework.",
+    author,
+    version,
+    long_about = None
+)]
 struct Opt {
     /// set the log level
     #[clap(short = 'l', long = "log", default_value = "info")]
